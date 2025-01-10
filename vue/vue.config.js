98acf1f56d6d669b32f1dec784016b5dc4bb4bed
Daniel Jablonski <daniel@jablonski.ovh>
2025-01-10 06:03:34 +0100

    allowedHosts all

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0', // Umożliwia nasłuchiwanie na wszystkich interfejsach
+    allowedHosts: 'all', // Akceptuje dowolny Host Header
+  }
 })
